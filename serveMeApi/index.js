0b10bccbe2fd966e43ddd3046605b248ee21d2ca
Vikas Vardhan <vikasvardhan@vikas-mbp.attlocal.net>
2020-02-07 09:55:06 -0600

    Initial Commit

---
+++ serveMeApi/index.js
@@ -0,0 +1,34 @@
+// var express = require("express");
+// var app = express();
+// var cors = require("cors");
+// app.use(cors);
+
+// var bodyParser = require("body-parser");
+
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
+
+// var port = process.env.PORT || 3330; // Set our port
+
+// // Routes for our API
+
+// app.get("/", (req, res) => {
+//   res.send("Hello World!");
+// });
+
+// app.listen(port, () =>
+//   console.log(`Hello world app listening on port ${port}!`)
+// );
+// console.log("Magic happens on port " + port);
+
+var express = require("express");
+var app = express();
+
+var cors = require("cors");
+app.use(cors());
+
+app.get("/", (req, res) => res.status(200).json({ message: "Hello World" }));
+
+app.listen(8585, () => {
+  console.log("listening on 8585");
+});
